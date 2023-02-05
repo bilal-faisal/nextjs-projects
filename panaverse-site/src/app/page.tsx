e6a9c99ff9eb7003e1b7e32f582fec7d9e60aa71
bilal-faisal <bilalfaisal.developer@gmail.com>
2023-02-05 20:57:07 +0500

    Integrated ChakraUI
    Created Panaverse Dao Next13 site and
    Integrated CharkaUI.

---
+++ panaverse-site/src/app/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import { Heading, Center } from "@chakra-ui/react";
+
+export default function Home() {
+  return (
+    <main>
+      <Center>
+        <Heading as="h1" size="lg" mt={2} mb={2}>
+          Panaverse Dao
+        </Heading>
+      </Center>
+    </main>
+  );
+}
